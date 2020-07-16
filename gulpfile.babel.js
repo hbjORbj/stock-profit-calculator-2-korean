875c3c5b239223d9c3afda406a75c0eeb673d77a
hbjORbj <sldisek783@gmail.com>
2020-07-16 12:31:34 +0900

    Done

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -4,7 +4,6 @@ import sass from "gulp-sass";
 import minify from "gulp-csso";
 import autoprefixer from "gulp-autoprefixer";
 import ghPages from "gulp-gh-pages";
-import image from "gulp-image";
 import bro from "gulp-bro";
 import babelify from "babelify";
 
@@ -20,10 +19,6 @@ const routes = {
     watch: "src/scss/*.scss",
     src: "src/scss/styles.scss",
     dest: "dist/css",
-  },
-  images: {
-    src: "src/images/*",
-    dest: "dist/images",
   },
   js: {
     watch: "src/js/*.js",
@@ -47,9 +42,6 @@ const styles = () =>
     .pipe(minify())
     .pipe(gulp.dest(routes.css.dest));
 
-const images = () =>
-  gulp.src(routes.images.src).pipe(image()).pipe(gulp.dest(routes.images.dest));
-
 const js = () =>
   gulp
     .src(routes.js.src)
@@ -71,7 +63,7 @@ const watch = () => {
 const clean = () => del(["dist", ".publish"]);
 
 const prepare = gulp.series([clean]);
-const assets = gulp.series([html, images, styles, js]);
+const assets = gulp.series([html, styles, js]);
 const live = gulp.parallel([watch]);
 
 export const build = gulp.series([prepare, assets]);
